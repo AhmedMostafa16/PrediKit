8201efe908f6a8e05fbcc67af5428cdb41db0323
MohanadElMetwally <mohanadelmetwally@gmail.com>
2024-02-23 16:27:39 +0200

    Add OnehotEncoder

--- predikit/preprocessing/_encoders.py
+++ predikit/preprocessing/_encoders.py
@@ -1,3 +1,6 @@
+import numpy as np
+from pandas import DataFrame
+
 from category_encoders import (
     BackwardDifferenceEncoder,
     BaseNEncoder,
@@ -9,11 +12,57 @@ from category_encoders import (
 )
 from sklearn.preprocessing import (
     LabelEncoder,
-    OneHotEncoder,
+    # OneHotEncoder,
     OrdinalEncoder,
 )
 
 from ._base import EncodingStrategies
+
+class OneHotEncoder:
+    def __init__(self, columns: list[str] = None, drop: str =None) -> None:
+        self.columns: list[str] = columns
+        self.encodings: dict[str, int] = {}
+        self.drop: str = drop
+        self.feature_names_out: list[str] = []
+
+    def fit(self, df) -> None:
+        if self.columns is None:
+            self.columns = df.columns
+        for column in self.columns:
+            unique_values = df[column].unique()
+            if self.drop == "first":
+                unique_values = np.delete(unique_values, 0)  # Drop the first category
+            elif self.drop == "if_binary" and len(unique_values) == 2:
+                unique_values = unique_values[1:]  # Drop the first category if binary
+            self.encodings[column] = {value: np.eye(len(unique_values))[i] for i, value in enumerate(unique_values)}
+            self.feature_names_out.extend([f"{column}_{value}" for value in unique_values])
+
+    def transform(self, df) -> DataFrame:
+        df_encoded = df
+        for column in self.columns:
+            if column in df_encoded:  # Check if column exists after dropping (if_binary)
+                for value, _ in self.encodings[column].items():
+                    df_encoded[column + '_' + str(value)] = (df_encoded[column] == value).astype(int)
+                df_encoded.drop(column, axis= 1, inplace = True)
+        return df_encoded
+
+    def fit_transform(self, df) -> DataFrame:
+        self.fit(df)
+        return self.transform(df)
+
+    def get_features_names_out(self) -> list[str]:
+        """
+        Returns the names of the encoded features.
+
+        This method should be called after fitting the encoder.
+
+        Returns:
+            list: A list of string, the names of the encoded features.
+        """
+        if self.feature_names_out is None:
+            raise ValueError("Please fit the encoder before calling get_features_names_out.")
+        return self.feature_names_out
+
 
 ENCODERS = {
     EncodingStrategies.HashingEncoder: HashingEncoder,
@@ -54,3 +103,4 @@ def init_encoder(strategy: EncodingStrategies, **params):
     if encoder is None:
         raise TypeError(f"Unsupported encoding strategy: {strategy}")
     return encoder(**params)
+
