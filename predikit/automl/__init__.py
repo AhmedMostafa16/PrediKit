067cd2ba49c07dd51da70b10026e4920772c145b
MohanadElMetwally <mohanadelmetwally@gmail.com>
2024-06-17 20:40:10 +0300

    -Add adjusted automl_h2o.py -Add adjusted __init__.py

--- predikit/automl/__init__.py
+++ predikit/automl/__init__.py
@@ -1,13 +1,14 @@
 from .automl_h2o import (
-    AutoML,
-    import_file,
-    initialize_cluster_server,
+    AutoML, 
+    initialize_cluster_server, 
+    import_file, 
     split_train_test,
-)
+    )
+
 
 __all__ = [
-    "AutoML",
-    "initialize_cluster_server",
-    "import_file",
-    "split_train_test",
-]
+    'AutoML', 
+    'initialize_cluster_server', 
+    'import_file', 
+    'split_train_test',
+    ]
